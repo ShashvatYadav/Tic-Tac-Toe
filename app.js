cf2aaec424df78bf964367d786f52fd517c1820f
ShashvatYadav <shashvtyadav945@gmail.com>
2024-10-19 13:12:03 +0530

    Add Draw Message

--- app.js
+++ app.js
@@ -17,22 +17,30 @@ let winPattern = [
 
 let turnO = true;
 let count = 0;
+
+// reset the game 
 let resetGame = () => {
     enableBoxes();
+    count = 0; // reset moves to 0
 }
 
 const disableBoxes = () =>{
+    // disable each boxes
     for(let box of boxes){
         box.disabled = true;
     }
 }
+// reset or start new game
 const enableBoxes = () =>{
-    msgContainer.classList.add("hide");
+    turnO = true;
+    msgContainer.classList.add("hide"); // hide the msg container
     for(let box of boxes){
         box.disabled = false;
         box.innerText = "";
     }
 }
+
+// go on each boxes 
 for(let box of boxes){
     box.addEventListener("click", () => {
         if(turnO){
@@ -44,34 +52,38 @@ for(let box of boxes){
             turnO = true;
             box.style.color = "#FFD54F";
         }
-        box.disabled = true;
-        count++;
-        findWinner();
+        box.disabled = true; // disable clicked button
+        count++; // move++
+        findWinner(); // check for winner
     });
 };
 
 let findWinner = () => {
-    let winner = false; 
+    let winner = false; // for draw 
     for(let pattern of winPattern){
+        // take winner pattern 
         const pat1 = pattern[0];
         const pat2 = pattern[1];
         const pat3 = pattern[2];
         if(boxes[pat1].innerText !== "" && boxes[pat2].innerText !== "" && boxes[pat3].innerText !== ""){
             if(boxes[pat1].innerText == boxes[pat2].innerText && boxes[pat2].innerText == boxes[pat3].innerText) {
                 msg.innerText = `Winner is ${boxes[pat1].innerText}`;
-                msgContainer.classList.remove("hide");
-                disableBoxes();
-                winner = true;
+                msgContainer.classList.remove("hide"); // show msg Container
+                disableBoxes(); // disable all the buttons 
+                winner = true; // no need to check draw 
                 return;
             } 
         }
     }
     // check Draw 
-    if(!winner && count === 9){
+    if(!winner && count == 9){
         msg.innerText = `Oops, Draw`;
         msgContainer.classList.remove("hide");
+        disableBoxes();
     }
 };
 
+// reset game
 resetBtn.addEventListener("click", resetGame);
+// start new game
 newBtn.addEventListener("click", resetGame);
